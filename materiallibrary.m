6f660bb32e835d586c9517e45d3f57060f7dc0bd
Alexander Valavanis <eenava@leeds.ac.uk>
2018-01-26 17:02:35 +0000

    Added loads of extra metals from M. A. Ordal et al., Appl. Opt. 24, p 4493 (1985)

--- materiallibrary.m
+++ materiallibrary.m
@@ -18,7 +18,76 @@ Si.eps_r = 11.6964;
 Si.set_caughey_thomas_params(460, 45, 2.23e17, -2.18, 3.2, 0.72);
 material_list('Si') = Si;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% METALS %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Set properties for Ag
+Ag = Material;
+Ag.set_plasma_params(9.01, 18.0);
+material_list('Ag') = Ag;
+
+%% Set properties for Al
+Al = Material;
+Al.set_plasma_params(14.8, 81.8);
+material_list('Al') = Al;
+
 %% Set properties for Au
 Au = Material;
 Au.set_plasma_params(9.02, 26.67);
 material_list('Au') = Au;
+
+%% Set properties for Co
+Co = Material;
+Co.set_plasma_params(3.97, 36.6);
+material_list('Co') = Co;
+
+%% Set properties for Cu
+Cu = Material;
+Cu.set_plasma_params(7.39, 9.08);
+material_list('Cu') = Cu;
+
+%% Set properties for Fe
+Fe = Material;
+Fe.set_plasma_params(4.09, 18.22);
+material_list('Fe') = Fe;
+
+%% Set properties for Mo
+Mo = Material;
+Mo.set_plasma_params(7.46, 51.08);
+material_list('Mo') = Mo;
+
+%% Set properties for Ni
+Ni = Material;
+Ni.set_plasma_params(4.88, 43.64);
+material_list('Ni') = Ni;
+
+%% Set properties for Pb
+Pb = Material;
+Pb.set_plasma_params(7.36, 202.1);
+material_list('Pb') = Pb;
+
+%% Set properties for Pd
+Pd = Material;
+Pd.set_plasma_params(5.46, 15.37);
+material_list('Pd') = Pd;
+
+%% Set properties for Pt
+Pt = Material;
+Pt.set_plasma_params(5.15, 69.2);
+material_list('Pt') = Pt;
+
+%% Set properties for Ti
+Ti = Material;
+Ti.set_plasma_params(2.52, 47.4);
+material_list('Ti') = Ti;
+
+%% Set properties for V
+V = Material;
+V.set_plasma_params(5.16, 60.6);
+material_list('V') = V;
+
+%% Set properties for W
+W = Material;
+W.set_plasma_params(6.41, 60.4);
+material_list('W') = W;
